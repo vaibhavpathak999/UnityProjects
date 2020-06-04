195b59b2f31f7a09ddccf429a1f6ea5f241fd45b
vaibhavpathak999 <vaibhavpathak999@gmail.com>
2020-06-04 17:39:49 +0530

    Update
    Lets see if it works

---
+++ MyJourneyFor2DBegins/Assets/UITextProgrammtically.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextProgrammtically : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    Text textComponent;
+
+    void Start()
+    {
+        textComponent.text = (" This game is gonna show you how to prevent ourselves from CORONA");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
