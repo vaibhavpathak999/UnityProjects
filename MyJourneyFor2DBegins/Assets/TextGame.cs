0894b106096a625171b5266332a07c282f837e1b
vaibhavpathak999 <vaibhavpathak999@gmail.com>
2020-06-05 01:22:03 +0530

    Commit

---
+++ MyJourneyFor2DBegins/Assets/TextGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state; //state is nothing but current state
+
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+            ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
